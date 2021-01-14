5e5460c9603d89f8496acbc8ac4c4edcfdae0538
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2021-01-15 00:00:24 +0100

    [AUTH]: remove unuseful code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
   <v-app id="app">
-
     <v-main>
       <router-view />
     </v-main>
-
   </v-app>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
 require("@/assets/styles/forms.css");
 
 export default {
-  created() {},
-  computed: {
-    ...mapGetters({
-      isAuthenticated: "auth/isAuthenticated",
-    }),
-  },
-
-  methods: {},
+  name: "App",
 };
 </script>
  

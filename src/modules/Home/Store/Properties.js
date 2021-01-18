78d4fc05031a33ac044b59948d0e47ab97593a68
Magnus <zelvekalois@outlook.com>
2021-01-18 13:50:06 +0100

    Add Get full

--- src/modules/Home/Store/Properties.js
+++ src/modules/Home/Store/Properties.js
@@ -20,7 +20,7 @@ export default {
         loadProperties({commit})
         {
             return http
-                .get(`properties`)
+                .get(`properties?groups%5B%5D=property_get_full`)
                 .then((response) => {
                     console.log(response.data);
                     commit('setProperties', response.data);

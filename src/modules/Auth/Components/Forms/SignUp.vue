96c685bd3f06bc04693da4ab965b5c2eed22cdbd
Bryan <gatay.bryan@gmail.com>
2021-01-19 23:36:39 +0100

    Add vuemik login and Yup verification

--- src/modules/Auth/Components/Forms/SignUp.vue
+++ src/modules/Auth/Components/Forms/SignUp.vue
@@ -1,71 +1,113 @@
 <template>
-  <Vuemik>
-  <form ref="registerForm">
-    <v-row>
-      <v-col cols="12" sm="6" md="6">
-        <label for="input__signup_fname" class="col-12">
-          First name
-        </label>
-        <Field
-         component="vuemik-text"
-         name="fname"
-         id="input__signup_fname"
-        />
-      </v-col>
-      <v-col cols="12" sm="6" md="6">
-        <label for="input__signup_lname" class="col-12">
-          Last name
-        </label>
-        <Field
-            component="vuemik-text"
-            name="lname"
-            id="input__signup_lname"
-        />
-      </v-col>
-      <v-col cols="12">
-        <label for="input__signup_email" class="col-12">
-          Email
-        </label>
-        <Field
-            component="vuemik-text"
-            name="email"
-            id="input__signup_email"
-        />
-      </v-col>
-      <v-col cols="12">
-        <label for="input__signup_passwd" class="col-12">
-          Password
-        </label>
-        <Field
-            component="vuemik-text"
-            name="password"
-            id="input__signup_passwd"
-        />
-      </v-col>
-      <v-col cols="12">
-        <label for="input__signup_confirm_passwd" class="col-12">
-          Confirm password
-        </label>
-        <Field
-            component="vuemik-text"
-            name="confirm_password"
-            id="input__signup_confirm_passwd"
-        />
-      </v-col>
-      <v-spacer></v-spacer>
-      <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
-        <v-btn x-large block color="success">Register</v-btn>
-      </v-col>
-    </v-row>
-  </form>
+  <Vuemik
+    :initial-values="initialValues"
+    :onSubmit="SignUpForm"
+    :validationSchema="SignUpschema"
+    v-slot="{ handleSubmit, errors }"
+  >
+    <form ref="registerForm">
+      <v-row>
+        <v-col cols="12">
+          <label for="input__signup_lname" class="col-12"> Last name </label>
+
+          <v-col cols="12" sm="6" md="6">
+            <Field
+              component="input"
+              name="lname"
+              id="input__signup_lname"
+              placeholder="Last name"
+            />
+            <p v-if="errors.lname" class="alert-error">{{ errors.lname[0] }}</p>
+          </v-col>
+          <label for="input__signup_fname" class="col-12"> First name </label>
+          <v-col cols="12" sm="6" md="6">
+            <Field
+              component="input"
+              name="fname"
+              id="input__signup_fname"
+              placeholder="First name"
+            />
+            <p v-if="errors.fname" class="alert-error">{{ errors.fname[0] }}</p>
+          </v-col>
+          <label for="input__signup_email" class="col-12"> Email </label>
+          <v-col cols="12" sm="6" md="6">
+            <Field
+              component="input"
+              name="email"
+              id="input__signup_email"
+              placeholder="Email"
+            />
+            <p v-if="errors.email" class="alert-error">{{ errors.email[0] }}</p>
+          </v-col>
+          <label for="input__signup_password" class="col-12"> Password </label>
+          <v-col cols="12" sm="6" md="6">
+            <Field
+              component="input"
+              name="password"
+              id="input__signup_password"
+              type="password"
+              placeholder="password"
+            />
+            <p v-if="errors.password" class="alert-error">
+              {{ errors.password[0] }}
+            </p>
+          </v-col>
+          <label for="input__signup_confirm_password" class="col-12">
+            Confirm Password
+          </label>
+          <v-col cols="12" sm="6" md="6">
+            <Field
+              component="input"
+              name="confirm_password"
+              id="input__signup_confirm_password"
+              type="password"
+              placeholder="Confirm password"
+            />
+            <p v-if="errors.confirm_password" class="alert-error">
+              {{ errors.confirm_password[0] }}
+            </p>
+          </v-col>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
+          <v-btn @click="handleSubmit" x-large block color="success"
+            >Register</v-btn
+          >
+        </v-col>
+      </v-row>
+    </form>
   </Vuemik>
 </template>
 
 <script>
-import Vuemik from "@/core/Components/Vuemik/Vuemik";
-import Field from "@/core/Components/Vuemik/Field";
+import * as Yup from "yup";
+import { Vuemik, Field } from "@/libs/vuemik";
 export default {
-name: "SignUpForm",
-  components: {Field, Vuemik}
-}
+  name: "SignUpForm",
+  components: { Field, Vuemik },
+  data: () => ({
+    initialValues: {},
+    SignUpschema: Yup.object().shape({
+      lname: Yup.string().required(),
+      fname: Yup.string().required(),
+      email: Yup.string().required().email(),
+      password: Yup.string().required().min(8),
+      confirm_password: Yup.string()
+      .oneOf([Yup.ref('password'), null])
+     .required('Password confirm is required')
+    }),
+  }),
+  methods: {
+    SignUpForm(props) {
+      console.log("props :>> ", props);
+    },
+  },
+};
 </script>
+<style  scoped>
+.alert-error {
+  margin-top: 10px;
+  color: red;
+}
+</style>
+

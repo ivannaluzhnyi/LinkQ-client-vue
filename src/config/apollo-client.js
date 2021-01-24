f381c8fdcac943e20dd56a824929e6dfc6ca406e
Magnus <zelvekalois@outlook.com>
2021-01-24 20:39:44 +0100

    Update all To newer version

--- src/config/apollo-client.js
+++ src/config/apollo-client.js
@@ -1,6 +1,6 @@
 const config = {
-    serverUri: process.env.APP_VUE_GQL || "http://localhost:4000/",
-    wsUsi: process.env.APP_VUE_GQLWS || "ws://localhost:4000",
+    serverUri: process.env.VUE_APP_GQL || "http://localhost:4000/",
+    wsUsi: process.env.VUE_APP_GQLWS || "ws://localhost:4000",
 };
 
 export default config;
